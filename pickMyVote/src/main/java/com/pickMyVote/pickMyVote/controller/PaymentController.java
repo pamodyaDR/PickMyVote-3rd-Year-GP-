4f83a844eb106b4d0f112a847fc7b58c7abf81ba
dhanushkagimhan <dhanushkagimhan@gmail.com>
2021-08-01 15:22:34 +0530

    created createPayment backend api

---
+++ pickMyVote/src/main/java/com/pickMyVote/pickMyVote/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.pickMyVote.pickMyVote.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pickMyVote.pickMyVote.model.Payment;
+import com.pickMyVote.pickMyVote.service.PaymentService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PaymentController {
+	
+	@Autowired
+	private PaymentService service;
+	
+	//create new election payment
+	@PostMapping("/createPayment")
+	public Payment createPayment(@RequestBody Payment payment) throws Exception{
+		
+		Payment paymentObj = null;
+		paymentObj = service.savePayment(payment);
+		return paymentObj;
+	}
+
+}
