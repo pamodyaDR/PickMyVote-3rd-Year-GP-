252f803cb70c442767fdd98536a69d965dd67c4a
dhanushkagimhan <dhanushkagimhan@gmail.com>
2021-08-13 13:31:43 +0530

    creating createElection part

--- ngPickMyVote/src/app/create-election/create-election.component.ts
+++ ngPickMyVote/src/app/create-election/create-election.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ElectionService } from '../election.service';
 
 @Component({
   selector: 'app-create-election',
@@ -12,12 +14,19 @@ import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 })
 export class CreateElectionComponent implements OnInit {
 
+  email = sessionStorage.getItem('session_username');
+  password = sessionStorage.getItem('session_password');
+
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) { }
+  constructor(private _service: ElectionService, private _router : Router, private _route: ActivatedRoute, private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    const id = this._route.snapshot.params['id'];
+    console.log(id);
+
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
