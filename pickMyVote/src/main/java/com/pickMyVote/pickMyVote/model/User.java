dd0fd7ab7c7d866dec9d9446f03b34284aa5c521
malshi-madurangi <m.madurangidesilva@gmail.com>
2021-08-13 13:23:57 +0530

    Updated (Registration and some other components)

--- pickMyVote/src/main/java/com/pickMyVote/pickMyVote/model/User.java
+++ pickMyVote/src/main/java/com/pickMyVote/pickMyVote/model/User.java
@@ -18,11 +18,17 @@ public class User {
     private String password;
     private String dob;
     private String roles;
+    private String country;
+    private String gender;
+    private String q1;
+    private String q2;
+    private String a1;
+    private String a2;
 
     public User() {
     }
 
-    public User(Long id, String f_name, String l_name, String email, int contact_num, String password, String dob) {
+    public User(Long id, String f_name, String l_name, String email, int contact_num, String password, String dob, String roles, String country, String gender, String q1, String q2, String a1, String a2) {
         this.id = id;
         this.f_name = f_name;
         this.l_name = l_name;
@@ -30,7 +36,13 @@ public class User {
         this.contact_num = contact_num;
         this.password = password;
         this.dob = dob;
-        this.roles="ROLE_USER";
+        this.roles= roles;
+        this.country = country;
+        this.gender = gender;
+        this.q1 = q1;
+        this.q2 = q2;
+        this.a1 = a1;
+        this.a2 = a2;
     }
 
     public String getRoles() {
@@ -96,5 +108,29 @@ public class User {
     public void setDob(String dob) {
         this.dob = dob;
     }
+
+    public String getCountry() { return country; }
+
+    public void setCountry(String country) { this.country = country; }
+
+    public String getGender() { return gender; }
+
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getQ1() { return q1; }
+
+    public void setQ1(String q1) { this.q1 = q1; }
+
+    public String getQ2() { return q2; }
+
+    public void setQ2(String q2) { this.q2 = q2; }
+
+    public String getA1() { return a1; }
+
+    public void setA1(String a1) { this.a1 = a1; }
+
+    public String getA2() { return a2; }
+
+    public void setA2(String a2) { this.a2 = a2; }
 }
 
