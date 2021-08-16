b5750b9a150a7570fb8692e8d8e1be493b965447
malshi-madurangi <m.madurangidesilva@gmail.com>
2021-08-16 23:26:40 +0530

    Admin home with new toolbar and sidenav
    ^adminhome.component.css, ^adminhome.component.html, ^adminhome.component.ts

--- ngPickMyVote/src/app/adminhome/adminhome.component.ts
+++ ngPickMyVote/src/app/adminhome/adminhome.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-adminhome',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminhomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
   }
 
 }
