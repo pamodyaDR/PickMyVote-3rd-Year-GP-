4b49b46391dba621f7187575c65c9b9e77cd1d97
dhanushkagimhan <dhanushkagimhan@gmail.com>
2021-08-19 15:00:51 +0530

    connect createElection to buyElectin btn

--- ngPickMyVote/src/app/elections/elections.component.ts
+++ ngPickMyVote/src/app/elections/elections.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-elections',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElectionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router : Router, private _route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+  }
+
+  goToCreateElection() {
+    const id = this._route.snapshot.params['id'];
+    this._router.navigate(['/createElection', id]);
   }
 
 }
